e4f7c73de729ca3d72f8749fa04020e5649c7cf8
Dikean <dylan01aponte@gmail.com>
2024-01-05 16:23:30 -0500

    good get first url json

--- src/models/usersCompanyModel.js
+++ src/models/usersCompanyModel.js
@@ -14,23 +14,23 @@ exports.findAll = async () => {
     }
 };
 
-    exports.userAsociateByCompany = async () => {
-        try {
-            // Preparar la consulta SQL sin datos específicos
-            const sql = `
-            SELECT u.*
-            FROM UsersCompany uc
-            JOIN Users u ON uc.UserID = u.UserID
-            WHERE uc.CompanyId = ?;
-            `;
-    
-            // Ejecutar la consulta preparada
-            const [results] = await db.query(sql); // Aquí no se pasan parámetros porque la consulta no los requiere
-    
-            return results;
-        } catch (error) {
-            throw error;
-        }
+exports.userAsociateByCompany = async () => {
+    try {
+        // Preparar la consulta SQL sin datos específicos
+        const sql = `
+        SELECT u.*
+        FROM UsersCompany uc
+        JOIN Users u ON uc.UserID = u.UserID
+        WHERE uc.CompanyId = ?;
+        `;
+
+        // Ejecutar la consulta preparada
+        const [results] = await db.query(sql); // Aquí no se pasan parámetros porque la consulta no los requiere
+
+        return results;
+    } catch (error) {
+        throw error;
+    }
 
 };
 
--- src/routes/usersCompanyRoutes.js
+++ src/routes/usersCompanyRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersCompanyController = require('../controllers/usersCompanyController');
 const checkJwt = require('../middleware/authMiddleware');
-const checkRole = require('../middleware/checkRoleMiddleware');
+// const checkRole = require('../middleware/checkRoleMiddleware');
 
 router.get('/UserCompany/:userId', checkJwt, usersCompanyController.getAsociateByUserCompany);
 
