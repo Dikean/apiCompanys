8da4eafd13a99c8d39c2f1af8da8b55da562ae48
Dikean <dylan01aponte@gmail.com>
2024-02-03 10:01:10 -0500

    Feature/DYLAN/03-02-2024/creating chatGPT

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const axios = require('axios');
+const { Configuration, OpenAI } = require('openai');
 // Cargar las variables de entorno
 dotenv.config();
 
@@ -23,6 +24,66 @@ app.use(cors());
 app.use('/api/userscompany', usersCompanyRoutes);
 app.use('/api/companys', companysController);
 
+app.get('/get-user-permissions', async (req, res) => {
+    const userId = "google-oauth2|108903769150858179970"; // Obtiene el userId de los parámetros de la ruta
+
+    // Configuración para obtener el token
+    var optionsToken = {
+        method: 'POST',
+        url: 'https://dev-w1j3tra2.us.auth0.com/oauth/token',
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+        data: new URLSearchParams({
+            grant_type: 'client_credentials',
+            client_id: 'in8vha00TxNfLFN1BbvQIOfCHm1h5SiB',
+            client_secret: 'IWA9OHErN3vYDQz4ydaMJl664NDU7plE0SBwAd8-_JcIkeA98YIJ-t-rAgNpv3dD', // Reemplaza esto con tu client_secret real
+            audience: 'https://dev-w1j3tra2.us.auth0.com/api/v2/'
+        })
+    };
+
+    try {
+        // Solicita el token de acceso
+        const tokenResponse = await axios.request(optionsToken);
+        const accessToken = tokenResponse.data.access_token;
+
+        // Configuración para obtener las permisiones del usuario con el token de acceso
+        var optionsPermissions = {
+            method: 'GET',
+            url: `https://dev-w1j3tra2.us.auth0.com/api/v2/users/${userId}/permissions`,
+            headers: {authorization: `Bearer ${accessToken}`}
+        };
+
+        // Solicita las permisiones del usuario
+        const permissionsResponse = await axios.request(optionsPermissions);
+        res.json(permissionsResponse.data); // Envía las permisiones como respuesta
+    } catch (error) {
+        console.error("Error en la solicitud: ", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+
+//gpt 
+
+// Configurar la API key y la organización (opcional)
+// const openai = new OpenAI({
+//   apiKey: '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', // Asegúrate de tener tu API key en una variable de entorno
+//   organization: 'org-rhsYIvVA1iplfBLSAMnhSEfM', // Opcional: especifica la organización si perteneces a varias
+// });
+
+
+const openai = new OpenAI(new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  }));
+  
+  async function main() {
+    const chatCompletion = await openai.chatCompletions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+    });
+    console.log(chatCompletion.data);
+  }
+
+  main();
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
